07430dc7072b2aba45440bcf026fcc9b9be9463a
Imad Jakhrouti <imadjakhrouti@gmail.com>
2024-03-31 00:52:14 +0000

    Add design and controller for login and sign up

--- private/views/home.view.php
+++ private/views/home.view.php
@@ -1,3 +1,3 @@
 <?php  $this->view("includes/head")?>
-<h1>page home</h1>
+<h1 class="text-red-900 text-3xl">page home</h1>
 <?php  $this->view("includes/footer")?>
--- private/views/includes/head.view.php
+++ private/views/includes/head.view.php
@@ -4,8 +4,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="http://localhost/school/public/assets/css/bootstrap.min.css">
+    <!-- <link rel="stylesheet" href="http://localhost/school/public/assets/css/bootstrap.min.css"> -->
+    <link rel="stylesheet" href="./../../../school/public/assets/css/output.css">
     <title>page web</title>
 </head>
 
 <body>
+    <div class="container">
