c241ec94934e6bebbaf9d718608823ac1b91b274
Imad Jakhrouti <imadjakhrouti@gmail.com>
2024-04-05 14:41:28 +0000

    Update constructor of App class in app.php
    - Changed the way the controller file is loaded with a variable to improve readability.
    - Moved the method for checking if a method exists in the controller to be within the condition for checking if the second URL segment exists.
    - Changed the assignment of $url to use the null coalescing operator.
    - Updated the assignment of $params to use the array\_values function to reset array keys.
    - Updated the call to call\_user\_func\_array to use the new variable for the controller file.

--- private/core/app.php
+++ private/core/app.php
@@ -4,35 +4,33 @@ class App
 {
     protected $controller = 'home';
     protected $method = 'index';
-    protected $params = array();
+    protected $params = [];
 
     public function __construct()
     {
         $url = $this->getURL();
-        if (file_exists("../private/controllers/" . $url[0] . ".php")) {
+
+        $controllerFile = "../private/controllers/" . $url[0] . ".php";
+        if (file_exists($controllerFile)) {
             $this->controller = ucfirst($url[0]);
             unset($url[0]);
         }
 
-        require "../private/controllers/" . $this->controller . ".php";
+        require $controllerFile;
         $this->controller = new $this->controller();
-        
-        if(isset($url[1])){
-            if(method_exists($this->controller , $url[1])){
-                $this->method = ucfirst($url[1]);
-                unset($url[1]);
-            }
+
+        if (isset($url[1]) && method_exists($this->controller, $url[1])) {
+            $this->method = ucfirst($url[1]);
+            unset($url[1]);
         }
 
-        $url = array_values($url);
-        $this->params = $url;
-        call_user_func_array([$this->controller,$this->method],$this->params);
+        $this->params = array_values($url);
+        call_user_func_array([$this->controller, $this->method], $this->params);
     }
-    
 
     private function getURL()
     {
-        $url = isset($_GET['url']) ? $_GET['url'] : 'home';
+        $url = $_GET['url'] ?? 'home';
         $url = trim($url, '/');
         $url = filter_var($url, FILTER_SANITIZE_URL);
         return explode("/", $url);
