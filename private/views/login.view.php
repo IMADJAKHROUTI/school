3179961b191816e2280b8c0bfc1d2a21ecf807cc
Imad Jakhrouti <imadjakhrouti@gmail.com>
2024-04-15 12:41:03 +0100

    Update Login and Signup Page with Input Validation Error Messages

--- private/views/login.view.php
+++ private/views/login.view.php
@@ -1,14 +1,24 @@
-<?php $this->view("includes/head") ?>
+<?php $this->view("includes/head"); ?>
 <div class="container-fluid h-full w-full">
-    <div class="m-auto max-w-sm shadow-md p-4">
-        <form action="" class="flex flex-col gap-3">
+    <div class="login m-auto card max-w-sm shadow-sm p-4">
+        <form action="" class="flex flex-col gap-3" method="post">
             <h1 class="">Bienvenue</h1>
-            <input type=" email" class="form-control" name="email" id="email" placeholder="Email">
-            <input type="password" class="form-control" name="pass" id="pass" placeholder="Mot de pass">
-            <input type="submit" class="form-control btn btn-primary" value="Connexion">
-
+            <div class="mb-3">
+                <input type="email" class="form-control" name="email" id="email" value="<?= get_var('email') ?>" placeholder="Email">
+                <?php if (isset($errors['email'])): ?>
+                    <span class="text-danger"><?= $errors['email'] ?></span>
+                <?php endif; ?>
+            </div>
+            <div class="mb-3">
+                <input type="password" class="form-control" name="password" id="password" value="<?= get_var('password')?>" placeholder="Mot de passe">
+                <?php if (isset($errors['password'])): ?>
+                    <span class="text-danger"><?= $errors['password'] ?></span>
+                <?php endif; ?>
+            </div>
+            <div class="mb-3">
+                <input type="submit" class="form-control btn btn-primary" value="Connexion">
+            </div>
         </form>
-
     </div>
 </div>
-<?php $this->view("includes/footer") ?>
+<?php $this->view("includes/footer"); ?>
--- private/views/signup.view.php
+++ private/views/signup.view.php
@@ -1,34 +1,63 @@
-<?php $this->view("includes/head") ?>
-<div class="container-fluid  h-full w-full">
-    <div class="m-auto max-w-sm shadow-md p-4">
-        <form action="" class="flex flex-col gap-3">
-            <h1 class="">Ajouter utilsateur</h1>
-            <input type="text" class="form-control" name="nom" id="nom" placeholder="Nom">
-            <input type="text" class="form-control" name="prenom" id="prenom" placeholder="Prenom">
-            <input type=" email" class="form-control" name="email" id="email" placeholder="Email">
-            <input type="text" class="form-control" name="pass1" id="pass1" placeholder="Mot de pass">
-            <input type="text" class="form-control" name="pass2" id="pass2" placeholder="Confirmer mot de pass">
-
-            <select name="" id="" class="form-control">
-                <option value="male">Male</option>
-                <option value="female">Female</option>
-            </select>
-            <select name="" id="" class="form-control">
-                <option value="student">Student</option>
-                <option value="reception">Reception</option>
-                <option value="prof">Professeur</option>
-                <option value="dep_admin">departement admin</option>
-                <option value="super_Admin">Super admin</option>
-            </select>
-
-
-
-            <input type="reset" class="form-control btn bg-red-600 text-white" value="Annuler">
-            <input type="submit" class="form-control btn btn-primary" value="Inscreption">
-
-
+<?php $this->view("includes/head"); ?>
+<div class="container-fluid h-full w-full">
+    <div class="signup m-auto col-lg-4 col-md-6 col-sm-8 shadow-sm p-4">
+        <form action="" method="post">
+            <h1 class="h3">Ajouter utilisateur</h1>
+            <div class="mb-3">
+                <input type="text" class="form-control" value="<?= get_var('nom') ?>" name="nom" id="nom" placeholder="Nom">
+                <?php if (isset($errors['nom'])): ?>
+                    <span class="text-danger"><?= $errors['nom'] ?></span>
+                <?php endif; ?>
+            </div>
+            <div class="mb-3">
+                <input type="text" class="form-control" value="<?= get_var('prenom') ?>" name="prenom" id="prenom" placeholder="Prenom">
+                <?php if (isset($errors['prenom'])): ?>
+                    <span class="text-danger"><?= $errors['prenom'] ?></span>
+                <?php endif; ?>
+            </div>
+            <div class="mb-3">
+                <input type="email" class="form-control" value="<?= get_var('email') ?>" name="email" id="email" placeholder="Email">
+                <?php if (isset($errors['email'])): ?>
+                    <span class="text-danger"><?= $errors['email'] ?></span>
+                <?php endif; ?>
+            </div>
+            <div class="mb-3">
+                <select name="genre" id="genre" class="form-control">
+                    <option <?= get_select("genre", "") ?> value="">-- SELECT genre --</option>
+                    <option <?= get_select("genre", "male") ?> value="male">Male</option>
+                    <option <?= get_select("genre", "female") ?> value="female">Female</option>
+                </select>
+                <?php if (isset($errors['genre'])): ?>
+                    <span class="text-danger"><?= $errors['genre'] ?></span>
+                <?php endif; ?>
+            </div>
+            <div class="mb-3">
+                <select name="role" id="role" class="form-control">
+                    <option <?= get_select("role", "") ?> value="">-- SELECT role --</option>
+                    <option <?= get_select("role", "etudiant") ?> value="etudiant">Etudiant</option>
+                    <option <?= get_select("role", "reception") ?> value="reception">Reception</option>
+                    <option <?= get_select("role", "professeur") ?> value="professeur">Professeur</option>
+                    <option <?= get_select("role", "admin") ?> value="admin">Admin</option>
+                    <option <?= get_select("role", "super_admin") ?> value="super_admin">Super_admin</option>
+                </select>
+                <?php if (isset($errors['role'])): ?>
+                    <span class="text-danger"><?= $errors['role'] ?></span>
+                <?php endif; ?>
+            </div>
+            <div class="mb-3">
+                <input type="password" class="form-control" value="<?= get_var('password') ?>" name="password" id="password" placeholder="Mot de passe">
+            </div>
+            <div class="mb-3">
+                <input type="password" class="form-control" value="<?= get_var('password2') ?>" name="password2" id="password2" placeholder="Confirmer mot de passe">
+                <?php if (isset($errors['password'])): ?>
+                    <span class="text-danger"><?= $errors['password'] ?></span>
+                <?php endif; ?>
+            </div>
+            <div class="mb-3">
+                <input type="reset" class="form-control btn btn-danger text-white" value="Annuler"> 
+                <input type="submit" class="form-control btn btn-primary text-white" value="Inscription">
+            </div>
         </form>
-
     </div>
 </div>
-<?php $this->view("includes/footer") ?>
+<?php $this->view("includes/footer"); ?>
