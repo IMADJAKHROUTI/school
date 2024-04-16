020e8fa2563df72fa95478d89ec7d10e17e9aaae
Imad Jakhrouti <imadjakhrouti@gmail.com>
2024-04-16 12:42:19 +0100

    Change die() to provide error message in Database.php

--- private/core/database.php
+++ private/core/database.php
@@ -13,32 +13,28 @@ class Database
             return $conn ;
         }
         catch(PDOException $e){
-            die("");
+            die("Erreur de connexion à la base de données : ".$e->getMessage());
         }
     }
     
     public function query($query,$data = array(),$data_type="object"){
         $conn = $this->connect();
         $stm = $conn->prepare($query);
-        if($stm){
+        if($stm !== false){
             $check = $stm->execute($data);
             if($check){
-                if($data_type=="object"){
+                if($data_type == "object"){
                     $data_F = $stm ->fetchAll(PDO::FETCH_OBJ);
                 }
                 else{
                     $data_F = $stm ->fetchAll(PDO::FETCH_ASSOC);
                 }
-                if(is_array($data_F) && count($data_F)>0){
+                if(is_array($data_F) && count($data_F) > 0){
                     return $data_F;
                 }
-            }
-            
+            } 
         }
-
-        
         return false;
-
     }
 
 }
